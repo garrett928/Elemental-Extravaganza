fc63532bdf6096f6bd39080b3c0394b6f6e02b22
Garrett Hart <garrett7621@gmail.com>
2019-10-20 23:37:50 -0400

    reads ps2 controller valsto variables

--- robot-code/robot-code.ino
+++ robot-code/robot-code.ino
@@ -1,36 +1,37 @@
 #include <PS2X_lib.h>
-#include <Psx.h>
-
 
 //playstation controller pinout
 #define PS_DATA_PIN 2
 #define PS_CMND_PIN 3
-#define PS_ATT_PIN 4
+#define PS_ATT_PIN 4 //sel pin for ps2 library
 #define PS_CLOCK_PIN 5
-#define PS_DELAY 10
 
-#define RIGHT_JOY_X 40 
-#define RIGHT_JOY_Y 48
+//#define pressures   true
+#define pressures   false
+//#define rumble      true
+#define rumble      false
 
-//ps controller object
-Psx controller;
+//ps2 controller object
+PS2X controller;
 
 //vars for seperated controller data
-byte controllerRightX;
-byte controllerRightY;
-int controllerLeftX;
-int controllerLeftY;
+float joyRightX;
+float joyRightY;
+float joyLeftX;
+float joyLeftY;
 
 //var raw controller data
 unsigned int controller_data;
 
 void setup() {
 //init controller pins
-controller.setupPins(PS_DATA_PIN, PS_CMND_PIN, PS_ATT_PIN,PS_CLOCK_PIN,PS_DELAY);
+controller.config_gamepad(PS_CLOCK_PIN, PS_CMND_PIN, PS_ATT_PIN, PS_DATA_PIN, pressures, rumble);
+
+//give controller time for wireless setup
+delay(300);
 
 //init serial
-Serial.begin(9600);
-
+Serial.begin(57600);
 }
 
 void loop() {
@@ -43,24 +44,27 @@ void loop() {
 
 */
 void readController(){
-  controller_data = controller.read();
+  //read gamepad to refresh library vals
+  controller.read_gamepad();
 
-  Serial.print("data1: ");
-  Serial.println(controller_data, BIN);
 
-  // controller_data = controller_data >> RIGHT_JOY_X;
-  byte tempLeftY = controller_data >> 8;
 
-  Serial.print("data2: ");
-  Serial.println(controller_data, DEC);
+  //update joystick's states
+  joyLeftX = controller.Analog(PSS_LX);
+  joyLeftY = controller.Analog(PSS_LY);
+  joyRightX = controller.Analog(PSS_RX);
+  joyRightY = controller.Analog(PSS_RY);
 
-   Serial.print("data3: ");
-  Serial.println(tempLeftY, DEC);
+ //needs to not floor vals
 
-  // controllerRightX >> 8;
+  //map joy vals to -1 to 1
+  joyLeftX = map(joyLeftX, 0, 255, -1, 1);
+  joyLeftY =  map(joyLeftY, 0, 255, -1, 1);
+  joyRightX =  map(joyRightX, 0, 255, -1, 1);
+  joyRightY =  map(joyRightY, 0, 255, -1, 1);
 
-  // Serial.print("data3: ");
-  // Serial.println(controllerLeftX, DEC);
+  Serial.print("left x");
+  Serial.println(joyLeftX);
 
-  delay(15);
+  delay(30);
 }
